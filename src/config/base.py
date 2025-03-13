6c22af697ea0e70dbcaceec95262d35b508ae84a
TsaplinIA <tsaplin.ia@outlook.com>
2025-03-13 13:47:44 +0300

    add dirs to config

--- src/config/base.py
+++ src/config/base.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 PROJECT_DIR = Path(__file__).parent.parent.parent
+TEMPLATE_DIR = PROJECT_DIR / "templates"
+STATIC_DIR = PROJECT_DIR / "static"
 
 APP_SECRET_KEY = 'change'
 SQLALCHEMY_DATABASE_URI = 'change'
