e2df71de5515528c5249a1ab035d5c0f5025dfa4
TsaplinIA <tsaplin.ia@outlook.com>
2025-03-17 17:43:49 +0300

    refactor BaseDBRepository

--- src/infra/repositories/base.py
+++ src/infra/repositories/base.py
@@ -1,40 +1,32 @@
+import logging
 from typing import Self
 
 from flask_sqlalchemy.session import Session
 from sqlalchemy.exc import SQLAlchemyError
 
+logger = logging.getLogger(__name__)
 
 class BaseDBRepository[T]:
     def __init__(self, session: Session, model: type[T]):
         self.session = session
         self.model = model  # Теперь можно передавать модель напрямую
 
-    def get(self, **kwargs) -> T | None:
+    def get_one_or_none(self, **kwargs) -> T | None:
         """Получает объект"""
         try:
             return self.session.query(self.model).filter_by(**kwargs).one_or_none()
         except SQLAlchemyError as e:
             raise RuntimeError(f"Database error while fetching {self.model.__name__}: {e}")
 
-    def save(self, obj: T) -> Self:
-        """Сохраняет изменения в объекте и возвращает self для чейнинга."""
-        if not isinstance(obj, self.model):
-            raise TypeError(f"Expected an instance of {self.model.__name__}")
+    def get_few(self, **kwargs) -> list[T]:
         try:
-            self.session.commit()
-            return self
+            return self.session.query(self.model).filter_by(**kwargs).all()
         except SQLAlchemyError as e:
-            self.session.rollback()
-            raise RuntimeError(f"Database error while saving {self.model.__name__}: {e}")
+            raise RuntimeError(f"Database error while fetching {self.model.__name__}: {e}")
 
     def remove(self, obj: T) -> Self:
         """Удаляет объект из БД и возвращает self для чейнинга."""
         if not isinstance(obj, self.model):
             raise TypeError(f"Expected an instance of {self.model.__name__}")
-        try:
-            self.session.delete(obj)
-            self.session.commit()
-            return self
-        except SQLAlchemyError as e:
-            self.session.rollback()
-            raise RuntimeError(f"Database error while removing {self.model.__name__}: {e}")
+        self.session.delete(obj)
+        return self
