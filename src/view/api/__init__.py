c8147c1460c0cbc4cc1284d1fa67f69b3c2499ca
TsaplinIA <tsaplin.ia@outlook.com>
2025-03-17 17:42:45 +0300

    dont use all in apis

--- src/view/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.view.api.new import new_api_router
-from src.view.api.players import players_api_router
-from src.view.api.rating import rating_api_router
-
-__all__ = [
-    'new_api_router',
-    'players_api_router',
-    'rating_api_router',
-]
