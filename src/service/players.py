6a1bdd6d9c5a0f1dc1c0b982b1ce7ea31b9aa324
TsaplinIA <tsaplin.ia@outlook.com>
2025-03-31 02:02:40 +0300

    add caching for get_players and get_player

--- src/service/players.py
+++ src/service/players.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy.session import Session
 from sqlalchemy.exc import IntegrityError
 
+from src.infra.chache import player_cache
 from src.infra.models import Player
 from src.infra.repositories.players import PlayerRepository
 from src.service.dto.errors import PlayerAlreadyExist
@@ -13,12 +14,22 @@ class PlayerService:
         self.players_repository = PlayerRepository(session)
 
     def get_player(self, **kwargs) -> PlayerModelShort | None:
-        db_player = self.players_repository.get_one_or_none(**kwargs)
-        return PlayerModelShort.from_db(db_player) if db_player else None
+        cache_key = frozenset(kwargs.items())
+        @player_cache.cache_on_arguments()
+        def _get(keys: frozenset) -> PlayerModelShort | None:
+            print(f"search player {kwargs=}")
+            db_player = self.players_repository.get_one_or_none(**dict(keys))
+            return PlayerModelShort.from_db(db_player) if db_player else None
+        return _get(cache_key)
 
     def get_players(self, **kwargs) -> list[PlayerModelShort]:
-        players = self.players_repository.get_few(**kwargs)
-        return [PlayerModelShort.from_db(p) for p in players]
+        cache_key = frozenset(kwargs.items())
+        @player_cache.cache_on_arguments()
+        def _get(keys: frozenset) -> list[PlayerModelShort]:
+            print(f"search players {kwargs=}")
+            players = self.players_repository.get_few(**dict(keys))
+            return [PlayerModelShort.from_db(p) for p in players]
+        return _get(cache_key)
 
     def create_player(self, nickname: str) -> PlayerModelShort:
         if self.get_player(nickname=nickname) is not None:
